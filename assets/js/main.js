5730fa7d589ba622c8d79fa99986f728384c95a9
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-08-23 20:14:19 +0600

    update desktop view

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,20 @@ document.addEventListener("DOMContentLoaded", function () {
     drag: "free",
     focus: "center",
     autoScroll: {
-      speed: 1,
+      speed: .8,
     },
   }).mount(window.splide.Extensions);
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var splideTwo = new Splide(".splideTwo", {
+    type: "loop",
+    perPage: 4,
+    arrows: false,
+    pagination: false,
+    drag: "free",
+    focus: "center",
+    autoScroll: {
+      speed: .8,
+    },
+  }).mount(window.splideTwo.Extensions);
 });
